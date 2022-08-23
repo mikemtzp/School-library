a121227a9a032cbda594f27d682976824ea23481
mikemtzp <mikemp8226@gmail.com>
2022-08-23 11:47:23 -0500

    Add decorator pattern

--- person_class.rb
+++ person_class.rb
@@ -1,12 +1,22 @@
-class Person
+require './nameable_class'
+require './base_decorator'
+require './capitalize_decorator'
+require './trimmer_decorator'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
@@ -17,3 +27,14 @@ class Person
     @age >= 18
   end
 end
+
+# mike = Person.new(12, 'Mike')
+# p mike
+# p mike.correct_name
+
+person = Person.new(22, 'maximilianus')
+p person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+p capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+p capitalized_trimmed_person.correct_name
