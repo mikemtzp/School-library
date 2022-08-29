731b8db11154fec3036363c328e323f31320d380
mikemtzp <mikemp8226@gmail.com>
2022-08-29 11:52:46 -0500

    Make refactoring

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ def main
   puts 'Welcome to School Library App!'
 
   while exit.zero?
-    print '
+    print "\n
     Please choose an option by entering a number:
     1 - List all books
     2 - List all people
@@ -15,7 +15,7 @@ def main
     5 - Create a rental
     6 - List all rentals for a given person id
     7 - Exit
-    '
+    \n"
     option = gets.chomp.to_i
     option == 7 ? exit = 1 : app.select_option(option)
   end
