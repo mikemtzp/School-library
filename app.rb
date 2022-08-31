7404ac5645ea8710c919dfb8242224fb5e086a2f
mikemtzp <mikemp8226@gmail.com>
2022-08-31 16:32:02 -0500

    Modify preserve data method

--- app.rb
+++ app.rb
@@ -13,27 +13,29 @@ class App
   def initialize
     @people = File.exist?('./people.json') ? JSON.parse(File.read('./people.json'), create_additions: true) : []
     @books = File.exist?('./books.json') ? JSON.parse(File.read('./books.json'), create_additions: true) : []
-    @rentals = if File.exist?('./rentals.json')
-                 JSON.parse(File.read('./rentals.json'), create_additions: true).map do |rental|
-                   load_rental(rental)
-                 end
-               else
-                 []
-               end
+    @rentals = File.exist?('./rentals.json') ? JSON.parse(File.read('./rentals.json'), create_additions: true) : []
+
+    # @rentals = if File.exist?('./rentals.json')
+    #              JSON.parse(File.read('./rentals.json'), create_additions: true).map do |rental|
+    #                load_rental(rental)
+    #              end
+    #            else
+    #              []
+    #            end
   end
 
   def select_option(option)
     case option
     when 1
-      list_all_books(books)
+      list_all_books(@books)
     when 2
-      list_all_people(people)
+      list_all_people(@people)
     when 3
       @people << create_person
     when 4
       @books << create_book
     when 5
-      @rentals << create_rental(books, people)
+      @rentals << create_rental(@books, @people)
     when 6
       list_rentals(rentals)
     else
@@ -41,15 +43,59 @@ class App
     end
   end
 
-  def preserve_data
-    File.write('books.json', JSON.generate(@books))
-    File.write('people.json', JSON.generate(@people))
-    File.write('rentals.json', JSON.generate(@rentals))
+  def books_data(books)
+    array_list = []
+    books.each do |item|
+      array_list << {
+        title: item.title,
+        author: item.author
+      }
+    end
+    array_list
   end
 
-  def load_rental(rental)
-    rental_person = @people.filter { |person| person.id == rental[:person_id] }.first
-    rental_book = @books.filter { |book| book.title == rental[:book_title] }.first
-    Rental.new(rental[:date], rental_book, rental_person)
+  def people_data(people)
+    array_list = []
+    people.each do |item|
+      array_list << {
+        name: item.name,
+        age: item.age,
+        type: item.class,
+        id: item.id
+      }
+    end
+    array_list
   end
+
+  def renals_data(rentals)
+    array_list = []
+    rentals.each do |item|
+      array_list << {
+        person: {
+          name: item.person.name,
+          age: item.person.age,
+          type: item.person.class,
+          id: item.person.id
+        },
+        book: {
+          title: item.book.title,
+          author: item.book.author
+        },
+        date: item.date
+      }
+    end
+    array_list
+  end
+
+  def preserve_data
+    File.write('books.json', JSON.pretty_generate(books_data(@books)))
+    File.write('people.json', JSON.pretty_generate(people_data(@people)))
+    File.write('rentals.json', JSON.pretty_generate(rentals_data(@rentals)))
+  end
+
+  # def load_rental(rental)
+  #   rental_person = @people.filter { |person| person.id == rental[:person.id] }.first
+  #   rental_book = @books.filter { |book| book.title == rental[:book.title] }.first
+  #   Rental.new(rental[:date], rental_book, rental_person)
+  # end
 end
